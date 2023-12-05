d2f3f029c219dbe58ab8041acdacb578871ecb64
Shunmurama <mura.faily72@gmail.com>
2023-12-05 22:54:00 +0000

    ~p92

--- test.rb
+++ test.rb
@@ -4,4 +4,13 @@ class SampleTest < Minitest::Test
   def test_sample
     assert_equal 'RUBY', 'ruby'.upcase
   end
+
+  def test_practice
+    refute 5 * 4 == 29
+  end
+
+  def test_capital
+    assert_equal 'RUBY', 'ruby'.capitalize
+  end
 end
+
